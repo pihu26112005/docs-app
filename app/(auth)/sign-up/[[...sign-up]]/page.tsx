d209b0d5d40f84ff82d93149d7eb22097b1ed497
pihu26112005 <161943145+pihu26112005@users.noreply.github.com>
2024-07-24 00:33:48 +0530

    Create page.tsx

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <main className="auth-page">
+        <SignUp />
+    </main>
+  )
+}
+
+export default Signup
